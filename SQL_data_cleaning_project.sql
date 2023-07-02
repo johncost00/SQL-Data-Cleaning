bf5234e6f2023b5e4325a226d7ff9721b363763d
John Cost <134548480+johncost00@users.noreply.github.com>
2023-07-02 16:59:22 -0500

    Add files via upload

---
+++ SQL_data_cleaning_project.sql
@@ -0,0 +1,139 @@
+-- Cleaning Data Using SQL Queries
+
+Select *
+from DataCleaningProject.dbo.NashvilleHousing
+
+
+
+-- Step 1: Changing SaleDate
+--- Adding additional column to output data without the time stamp
+
+Alter Table NashvilleHousing 
+Add SalesDateConverted Date;
+
+Update NashvilleHousing
+Set SalesDateConverted = CONVERT(date,SaleDate)
+
+
+
+-- Step 2: Populate property address data
+--- In this table, there were some real estate properties that were listed more than once in the parcelID column
+---- Some rows were missing the property address because of this
+----- Here's how I cleaned the data in this situation:
+
+Select *
+from NashvilleHousing
+Select a.ParcelID, a.PropertyAddress, b.ParcelID, b.PropertyAddress, ISNULL(a.PropertyAddress,b.PropertyAddress)
+From DataCleaningProject.dbo.NashvilleHousing a
+JOIN DataCleaningProject.dbo.NashvilleHousing b
+	on a.ParcelID = b.ParcelID
+	AND a.[UniqueID ] <> b.[UniqueID ]
+Where a.PropertyAddress is null
+
+Update a
+SET PropertyAddress = ISNULL(a.PropertyAddress,b.PropertyAddress)
+From DataCleaningProject.dbo.NashvilleHousing a
+JOIN DataCleaningProject.dbo.NashvilleHousing b
+	on a.ParcelID = b.ParcelID
+	AND a.[UniqueID ] <> b.[UniqueID ]
+Where a.PropertyAddress is null
+
+
+
+-- Step 3: Breaking out Address into Individual Columns (Address, City, State)
+--- Splitting columns so that the street address, city, and state are in seperate columns
+
+SELECT
+SUBSTRING(PropertyAddress, 1, CHARINDEX(',', PropertyAddress) -1 ) as Address
+, SUBSTRING(PropertyAddress, CHARINDEX(',', PropertyAddress) + 1 , LEN(PropertyAddress)) as Address
+From DataCleaningProject.dbo.NashvilleHousing
+
+
+ALTER TABLE NashvilleHousing
+Add PropertySplitAddress Nvarchar(255)
+
+Update NashvilleHousing
+SET PropertySplitAddress = SUBSTRING(PropertyAddress, 1, CHARINDEX(',', PropertyAddress) -1 )
+
+
+ALTER TABLE NashvilleHousing
+Add PropertySplitCity Nvarchar(255);
+
+Update NashvilleHousing
+SET PropertySplitCity = SUBSTRING(PropertyAddress, CHARINDEX(',', PropertyAddress) + 1 , LEN(PropertyAddress))
+
+Select
+PARSENAME(REPLACE(OwnerAddress, ',', '.') , 3)
+,PARSENAME(REPLACE(OwnerAddress, ',', '.') , 2)
+,PARSENAME(REPLACE(OwnerAddress, ',', '.') , 1)
+From DataCleaningProject.dbo.NashvilleHousing
+
+ALTER TABLE NashvilleHousing
+Add OwnerSplitAddress Nvarchar(255);
+
+Update NashvilleHousing
+SET OwnerSplitAddress = PARSENAME(REPLACE(OwnerAddress, ',', '.') , 3)
+
+ALTER TABLE NashvilleHousing
+Add OwnerSplitCity Nvarchar(255);
+
+Update NashvilleHousing
+SET OwnerSplitCity = PARSENAME(REPLACE(OwnerAddress, ',', '.') , 2)
+
+ALTER TABLE NashvilleHousing
+Add OwnerSplitState Nvarchar(255);
+
+Update NashvilleHousing
+SET OwnerSplitState = PARSENAME(REPLACE(OwnerAddress, ',', '.') , 1)
+
+
+
+-- Step 4: Change Y and N to Yes and No in "Sold as Vacant" column
+
+Select SoldAsVacant
+, CASE When SoldAsVacant = 'Y' THEN 'Yes'
+	   When SoldAsVacant = 'N' THEN 'No'
+	   ELSE SoldAsVacant
+	   END
+From DataCleaningProject.dbo.NashvilleHousing
+
+Update NashvilleHousing
+SET SoldAsVacant = CASE When SoldAsVacant = 'Y' THEN 'Yes'
+	   When SoldAsVacant = 'N' THEN 'No'
+	   ELSE SoldAsVacant
+	   END
+
+
+
+-- Step 5: Remove Duplicates
+
+WITH RowNumCTE AS(
+Select *,
+	ROW_NUMBER() OVER (
+	PARTITION BY ParcelID,
+				 PropertyAddress,
+				 SalePrice,
+				 SaleDate,
+				 LegalReference
+				 ORDER BY
+					UniqueID
+					) row_num
+
+From DataCleaningProject.dbo.NashvilleHousing
+)
+delete
+From RowNumCTE
+Where row_num > 1
+
+
+
+-- Hello!
+--- Thank you for viewing my data cleaning project!
+---- I would like to add that I would remove the unused columns that I split up on step 3, but I'd like to keep the originals so you can view how I maniupulated the data
+----- If I were to remove the unused columns, this is how I'd do it:
+
+----------ALTER TABLE DataCleaningProject.dbo.NashvilleHousing
+----------DROP COLUMN OwnerAddress, TaxDistrict, PropertyAddress, SaleDate
+
+-- Please provide me any feedback or questions!
+
